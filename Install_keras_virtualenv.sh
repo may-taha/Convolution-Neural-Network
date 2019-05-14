2ff9f5627a89039076764dc9a5a8b4d5885e9673
May TAHA <49744578+may-taha@users.noreply.github.com>
2019-05-14 14:05:10 +0200

    Update Install_keras_virtualenv.sh

--- Install_keras_virtualenv.sh
+++ Install_keras_virtualenv.sh
@@ -5,19 +5,30 @@
 ## Assistants: Morgan Soulié Enrique Ortega-Abboud
 
 
+## INDEX
+## 1- Packages required from the system
+## 2- Creation of a virtual envirionment for python
+## 3- Install keras, dependencies and datasets
+## 4- Trouble shooting
+
+
+
+
 ################################################
-## Installation of packages in your system require administrative rights
+##  1- INSTALLATION OF PACKAGES IN YOUR SYSTEM REQUIRE ADMINISTRATIVE RIGHTS
 ################################################
 
 ## sudo apt-get install python-dev python-tk virtualenv
 
-## To install python 3.5 or 3.6 you should compile it and install it not to
-## mess with the system version of python
+## To install python 3.5 or 3.6 you should compile it
+## and install in a custom location.
+## Don't mess with the system version of python.
+## Use the PATH to the executable of python in the next section 
 
 
 
 ################################################
-## CREATE A VIRTUAL ENVIRONMENT
+## 2- CREATE A VIRTUAL ENVIRONMENT
 ################################################
 
 ## Check if virtualenv is installed, else install it
@@ -28,12 +39,18 @@ mkdir ~/envs
 cd ~/envs
 
 ## Find version of python (py 3.5 or 3.6 depending on your system)
-which python3.6
+## Use the full path of the executable, don't use the symbolic links
+path_to_python=$(ls -lS $(which python3.6)* | head -1 | awk '{print $NF}')
+
+## or use your custom path:
+## path_to_python=/usr/local/bin/python3.6
+
+
 
 ## Create the virtual environment with a given version of python
 ## Use that path in the parameter -p
 ## name your environment along with the path
-virtualenv -p /usr/local/bin/python3.6 ~/envs/pykeras
+virtualenv -p $path_to_python ~/envs/pykeras
 
 ## Start the environment
 source ~/envs/pykeras/bin/activate
@@ -43,7 +60,7 @@ source ~/envs/pykeras/bin/activate
 
 
 ################################################
-## CHECK PYTHON VERSION AND INSTALL PACKAGES FOR ML IN KERAS
+## 3- CHECK PYTHON VERSION AND INSTALL PACKAGES FOR ML IN KERAS
 ################################################
 
 
@@ -87,5 +104,16 @@ exit()"
 
 ## Once the work is done you can deactivate the environment or close the terminal
 deactivate
+
+
+
+################################################
+## 4- TROUBLE SHOOTING
+################################################
+
+## If you have any related problem with permissions of ~.cache/
+## you can remove the folder and run this script again.
+## You may need to sudo
+rm -rf ~.cache/
 
 
