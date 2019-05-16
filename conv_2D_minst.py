18e2897babaa54cd7d314134fc7e5dbc3b8eaf4b
May TAHA <49744578+may-taha@users.noreply.github.com>
2019-05-16 09:54:22 +0200

    Update conv_2D_minst.py

--- conv_2D_minst.py
+++ conv_2D_minst.py
@@ -7,7 +7,11 @@ from keras.layers import Conv2D, MaxPooling2D
 from keras.callbacks import ModelCheckpoint, EarlyStopping,  History
 import time
 from keras import backend as K
+import matplotlib
+matplotlib.use('Agg')
 from matplotlib import pyplot as plt
+plt.style.use('ggplot')
+
 
 batch_size = 128
 num_classes = 10
@@ -92,7 +96,7 @@ checkpointer = ModelCheckpoint(filepath="model_mnist.hdf5",
 
 earlystop = EarlyStopping(monitor = 'val_loss', patience=3)
 
-#Model saummary 
+#Model summary 
 print(model.summary())
 
 ###training 
@@ -114,11 +118,31 @@ D = time_end  - time_start
 score = model.evaluate(x_test, y_test, verbose=0)
 
 
+####A function that can be used to plot training and validation error 
+def plot_history(history):
+    acc = history.history['acc']
+    val_acc = history.history['val_acc']
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+    x = range(1, len(acc) + 1)
+    plt.figure(figsize=(12, 5))
+    plt.subplot(1, 2, 1)
+    plt.plot(x, acc, 'b', label='Training acc')
+    plt.plot(x, val_acc, 'r', label='Validation acc')
+    plt.title('Training and validation accuracy')
+    plt.legend()
+    plt.subplot(1, 2, 2)
+    plt.plot(x, loss, 'b', label='Training loss')
+    plt.plot(x, val_loss, 'r', label='Validation loss')
+    plt.title('Training and validation loss')
+    plt.legend()
+
+
 
 ###Training validation plot using the defined function
 plt.clf()
 plot_history(hist_model_mnist)
-plt.show()
+plt.savefig('training_and_validation_conv_2D_minst.png', bbox_inches='tight')
 
 
 
